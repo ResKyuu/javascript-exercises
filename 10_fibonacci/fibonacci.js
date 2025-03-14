510fa2c762423278cfefa2233836017189f60722
ResKyuu <73728388+ResKyuu@users.noreply.github.com>
2025-03-14 15:43:56 +0000

    Task 10 Fibonicci complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(counter) {
+    counter = parseInt(counter);
 
+    // base case: if num is 0 or 1, return num
+    if (counter < 0) return "OOPS";
+    if (counter === 0) return counter;
+    if (counter === 1) return counter;
+
+    return fibonacci(counter - 1) + fibonacci(counter - 2);
 };
 
 // Do not edit below this line
