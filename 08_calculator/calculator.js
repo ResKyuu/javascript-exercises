032dbf715e97effd4cdfa9365be170178fac2c92
ResKyuu <73728388+ResKyuu@users.noreply.github.com>
2025-03-14 14:15:39 +0000

    Task 08, calculator completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(num_1, num_2) {
+	return num_1 + num_2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num_1, num_2) {
+	return num_1 - num_2;
 };
 
-const sum = function() {
-	
+const sum = function(sumArray) {
+	return sumArray.reduce((a, b) => a + b, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(multiplyArray) {
+    return multiplyArray.reduce((a, b) => a * b);
 };
 
-const power = function() {
-	
+const power = function(num_1, num_2) {
+	return Math.pow(num_1, num_2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num_1) {
+    let total = num_1;
+	for (let i = 1; i < num_1; ++i) {
+      total *= i;
+    }
+    return total === 0 ? 1 : total;
+
+
 };
 
 // Do not edit below this line
