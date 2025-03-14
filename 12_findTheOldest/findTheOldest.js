bbd71915cebc38512187ef2acc41ab167968b2ae
ResKyuu <73728388+ResKyuu@users.noreply.github.com>
2025-03-14 16:44:24 +0000

    Task 12, partially finished. Futher completion coming soon

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
+const {sum} = require("../08_calculator/calculator");
+const findTheOldest = function(people) {
+    const yearsLived = people.sort((a, b) => {
+            if (a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth) return -1;
 
+    });
+    return yearsLived[0];
 };
 
 // Do not edit below this line
